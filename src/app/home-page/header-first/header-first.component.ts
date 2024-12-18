c6f41823c23ba61957562a1f1d99d62858d65166
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-12-18 14:55:31 -0400

    fiz container-download-app-floating e container-notification-floating

--- src/app/home-page/header-first/header-first.component.ts
+++ src/app/home-page/header-first/header-first.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrl: './header-first.component.scss'
 })
 export class HeaderFirstComponent {
+  showContainerDownloadAppFloating = false;
+  showContainerNotification = false;
 
+  settimeOutAny: any
+  settimeOutNotification: any
+
+  constructor(){
+  }
+
+  onMouseEnterDownloadApp(){
+    this.showContainerDownloadAppFloating = true;
+  }
+
+  onMouseLeaveDownloadApp(){
+    clearTimeout(this.settimeOutAny);
+
+    this.settimeOutAny = setTimeout(() => {
+      this.showContainerDownloadAppFloating = false;
+    }, 100);
+  }
+
+  onMouseEnterDownloadAppFloating(){
+    clearTimeout(this.settimeOutAny);
+    this.showContainerDownloadAppFloating = true;
+  }
+
+  onMouseLeaveDownloadAppFloating(){
+    this.showContainerDownloadAppFloating = false;
+  }
+
+  onMouseEnterNotification(){
+    this.showContainerNotification = true;
+  }
+
+  onMouseLeaveNotification(){
+    clearTimeout(this.settimeOutNotification);
+
+    this.settimeOutNotification = setTimeout(() => {
+      this.showContainerNotification = false;
+    }, 100);
+  }
+
+  onMouseEnterNotificationFloating(){
+    clearTimeout(this.settimeOutNotification);
+    this.showContainerNotification = true;
+  }
+
+  onMouseLeaveNotificationFloating(){
+    console.log("onMouseLeaveNotificationFloating");
+
+    this.showContainerNotification = false;
+  }
+
+  onMouseEnterArrowTopFloating(){
+    clearTimeout(this.settimeOutNotification);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.settimeOutAny);
+    clearTimeout(this.settimeOutNotification);
+  }
 }
