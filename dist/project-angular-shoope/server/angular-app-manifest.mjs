c284dfe659f590b408d9556bd0442a8d7e950172
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-28 16:01:11 -0400

    criando uma nova build 5.0

--- dist/project-angular-shoope/server/angular-app-manifest.mjs
+++ dist/project-angular-shoope/server/angular-app-manifest.mjs
@@ -14,31 +14,31 @@ export default {
   ],
   "src/app/user-my-account-all-info/user-my-account-all-info.module.ts": [
     {
-      "path": "chunk-RSAXU723.js",
+      "path": "chunk-FJMM2GOP.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-6CC2GIDL.js",
+      "path": "chunk-WE4UVPLH.js",
       "dynamicImport": true
     },
     {
-      "path": "chunk-ZS2L66BA.js",
+      "path": "chunk-54CEX2ML.js",
       "dynamicImport": true
     },
     {
-      "path": "chunk-YNUP564D.js",
+      "path": "chunk-FFONSCBB.js",
       "dynamicImport": true
     },
     {
-      "path": "chunk-2ZHSYJTU.js",
+      "path": "chunk-76VOYDIN.js",
       "dynamicImport": true
     },
     {
-      "path": "chunk-ZI64RW5W.js",
+      "path": "chunk-I3T3RUYW.js",
       "dynamicImport": true
     },
     {
-      "path": "chunk-T5NK6HNW.js",
+      "path": "chunk-MLBMVMUP.js",
       "dynamicImport": true
     },
     {
@@ -48,13 +48,13 @@ export default {
   ],
   "src/app/flash-sale/flash-sale.module.ts": [
     {
-      "path": "chunk-5QJRYLOT.js",
+      "path": "chunk-5ZS7JICW.js",
       "dynamicImport": false
     }
   ],
   "src/app/login-and-register-user/login-and-register-user.module.ts": [
     {
-      "path": "chunk-N5YC5X6N.js",
+      "path": "chunk-WL5B375H.js",
       "dynamicImport": false
     },
     {
@@ -64,7 +64,7 @@ export default {
   ],
   "src/app/user-my-account-all-info/my-account-all-component/my-account-all-component.module.ts": [
     {
-      "path": "chunk-6CC2GIDL.js",
+      "path": "chunk-WE4UVPLH.js",
       "dynamicImport": false
     },
     {
@@ -72,64 +72,64 @@ export default {
       "dynamicImport": false
     },
     {
-      "path": "chunk-C77I6VIG.js",
+      "path": "chunk-SJ6LJPNC.js",
       "dynamicImport": false
     }
   ],
   "src/app/user-my-account-all-info/setting-user/setting-user.module.ts": [
     {
-      "path": "chunk-ZS2L66BA.js",
+      "path": "chunk-54CEX2ML.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-C77I6VIG.js",
+      "path": "chunk-SJ6LJPNC.js",
       "dynamicImport": false
     }
   ],
   "src/app/user-my-account-all-info/purchase/purchase.module.ts": [
     {
-      "path": "chunk-YNUP564D.js",
+      "path": "chunk-FFONSCBB.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-C77I6VIG.js",
+      "path": "chunk-SJ6LJPNC.js",
       "dynamicImport": false
     }
   ],
   "src/app/user-my-account-all-info/notifications/notifications.module.ts": [
     {
-      "path": "chunk-2ZHSYJTU.js",
+      "path": "chunk-76VOYDIN.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-C77I6VIG.js",
+      "path": "chunk-SJ6LJPNC.js",
       "dynamicImport": false
     }
   ],
   "src/app/user-my-account-all-info/my-cupons-components/my-cupons-components.module.ts": [
     {
-      "path": "chunk-ZI64RW5W.js",
+      "path": "chunk-I3T3RUYW.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-C77I6VIG.js",
+      "path": "chunk-SJ6LJPNC.js",
       "dynamicImport": false
     }
   ],
   "src/app/user-my-account-all-info/my-shopee-coins-component/my-shopee-coins-component.module.ts": [
     {
-      "path": "chunk-T5NK6HNW.js",
+      "path": "chunk-MLBMVMUP.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-C77I6VIG.js",
+      "path": "chunk-SJ6LJPNC.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 63766, hash: '608cfa36d8c5dc49e998432a6f3af383be589ed604ded1cb2850b9d97ea98b0b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 63943, hash: '34d1ab8609f3ee008b4283c5689826e821fc562e6c1dff6bd0073ffcb5cdfdba', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 63766, hash: '648774986b2b30e0717829e0a68a64fbf952fa83ee1ff686550fb244dabb685e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 63943, hash: 'b09ee5a18d8b1119110db3db6934e3829f817355cb81ec14c92527faaa29dafa', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-SKS3PTMD.css': {size: 321, hash: 'WLFyag+OchI', text: () => import('./assets-chunks/styles-SKS3PTMD_css.mjs').then(m => m.default)}
   },
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environmentDev = {
   production: true,
-  BASE_URL: '', // Definindo a URL de produção
-  KEY_USER: ''
+  BASE_URL: 'https://shopee-backend-java-spring-production.up.railway.app/v1', // Definindo a URL de produção
+  KEY_USER: 'your-secret-key-here'
 };
