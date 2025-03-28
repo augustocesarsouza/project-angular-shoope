8d89c3917ceea0484d523c3fdfc41476a4cc80b5
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-28 15:21:31 -0400

    removendo environment prod

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environmentDev = {
-  production: true,
-  BASE_URL: 'https://shopee-backend-java-spring-production.up.railway.app/v1', // Definindo a URL de produção
-  KEY_USER: 'your-secret-key-here'
-};
